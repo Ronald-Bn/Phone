c56fcc9f618e9565123dec41326923946d046268
Ronnel <83863770+Babailan@users.noreply.github.com>
2021-08-12 21:06:32 +0800

    Phone

---
+++ app/src/main/java/com/example/phone/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.example.phone;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private String email, password;
+    private final String URL = "http://192.168.254.105/android/login.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email = password = "";
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+    }
+
+    public void login(View view){
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        if(!email.equals("") && !password.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(LoginActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(LoginActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected @NotNull Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email", email);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else{
+            Toast.makeText(this, "Field can not be empty!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void register(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/phone/RegisterActivity.java
@@ -0,0 +1,68 @@
+package com.example.phone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText etfullname , etphonenum, etemail, etpassword, etconfirmpassword, etaddress, etzipcode;
+    private Button btn_register;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        etfullname = findViewById(R.id.EditTextFullName);
+        etphonenum = findViewById(R.id.EditTextPhoneNum);
+        etemail = findViewById(R.id.EditTextRegisterEmail);
+        etpassword = findViewById(R.id.EditTextPassword);
+        etconfirmpassword = findViewById(R.id.EditTextConfirmPassword);
+        etaddress = findViewById(R.id.EditTextAddress);
+        etzipcode = findViewById(R.id.EditTextZipCode);
+        btn_register = findViewById(R.id.Btn_Register);
+        checkBox = findViewById(R.id.checkBox);
+
+
+
+       btn_register.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if (TextUtils.isEmpty(etfullname.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Full Name", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etphonenum.getText()) || etphonenum.getText().length() != 11) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Phone Number", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etemail.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etpassword.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Password", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etconfirmpassword.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Confirm Password", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etaddress.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Address", Toast.LENGTH_SHORT).show();
+               } else if (TextUtils.isEmpty(etzipcode.getText())) {
+                   Toast.makeText(RegisterActivity.this, "Please Enter Your Zip Code", Toast.LENGTH_SHORT).show();
+               }else if (!etpassword.getText().toString().trim().equals(etconfirmpassword.getText().toString().trim())) {
+                   Toast.makeText(RegisterActivity.this, "The Password is Not Match", Toast.LENGTH_SHORT).show();
+               }else {
+                   Intent intent = new Intent(RegisterActivity.this , MainActivity.class);
+                   intent.putExtra("fullname", etfullname.getText().toString());
+                   intent.putExtra("phonenum", etphonenum.getText().toString());
+                   intent.putExtra("email", etemail.getText().toString());
+                   intent.putExtra("password", etpassword.getText().toString());
+                   intent.putExtra("address", etaddress.getText().toString());
+                   intent.putExtra("zipcode", etzipcode.getText().toString());
+                   startActivity(intent);
+               }
+           }
+       });
+    }
+}
