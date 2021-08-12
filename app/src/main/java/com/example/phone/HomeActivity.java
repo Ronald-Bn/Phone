206c2cced6451eea836a17d6d94b626d3a394d48
Ronnel <83863770+Babailan@users.noreply.github.com>
2021-08-12 18:19:45 +0800

    Phone

---
+++ app/src/main/java/com/example/phone/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.example.phone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    private TextView usernumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button button = findViewById(R.id.logoutBtn);
+        usernumber = findViewById(R.id.phoneTv);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUserStatus();
+
+        button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+            Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    });
+}
+
+    private void checkUserStatus() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            String UserId = firebaseUser.getUid();
+            usernumber.setText(UserId);
+        }else{
+            finish();
+        }
+    }
+}
