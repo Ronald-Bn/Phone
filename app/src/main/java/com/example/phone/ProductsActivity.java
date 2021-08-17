31ebdf288ec187e6802d2d01bc87615517e93175
Ronnel <83863770+Babailan@users.noreply.github.com>
2021-08-17 23:06:33 +0800

    Phone

--- app/src/main/java/com/example/phone/ProductsActivity.java
+++ app/src/main/java/com/example/phone/ProductsActivity.java
@@ -13,13 +13,11 @@ public class ProductsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
         AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.avail, android.R.layout.simple_dropdown_item_1line);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-// Apply the adapter to the spinner
         autoCompleteTextView.setAdapter(adapter);
     }
 }
